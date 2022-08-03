8154872b161b9c7ca11bb75fe7aa3ffb8d94f0b0
KAUSHIK BAIDYA <47146123+KaushikBaidya@users.noreply.github.com>
2022-08-03 10:35:56 +0600

    Update Footer.js

--- src/components/frontend/layout/Footer.js
+++ src/components/frontend/layout/Footer.js
@@ -7,23 +7,23 @@ const Footer = () => {
     <>
       <div className="w-full grid grid-cols-1 justify-items-center py-10 bg-white">
         <div className="flex justify-center items-center">
-          <img src="/images/logo.png" alt="" className="h-12 ml-5" />
+          <img src="/images/logo.png" alt="" className="h-16 ml-5" />
           <h1 className="text-[#585858] font-semibold text-lg lg:text-2xl">
             TVC Vietnam
           </h1>
         </div>
 
-        <div className="w-full grid grid-cols-1 md:grid-cols-3 justify-items-center gap-y-2 py-2">
+        <div className="w-full grid grid-cols-1 md:grid-cols-3 justify-items-center gap-y-2 py-2 mt-2">
           <div>
             <p className="text-sm lg:text-lg text-[#201A59] font-semibold font-sans text-center px-5 ">
               Address:
-              <span className="font-normal">9 New Road , London , E1 1HE</span>
+              <span className="font-normal">Haiphong, Vietnam</span>
             </p>
             <p className="text-sm lg:text-lg text-[#201A59] font-semibold font-sans text-center px-5 ">
               Email:
-              <span className="font-normal">info@zajeducation.co.uk</span>
+              <span className="font-normal"> info@tvcvietnam.com</span>
             </p>
-            <p className="text-lg text-[#201A59] font-semibold font-sans text-center px-5 ">
+            {/* <p className="text-lg text-[#201A59] font-semibold font-sans text-center px-5 ">
               Contact Number
             </p>
             <p className="text-sm lg:text-lg text-[#201A59] font-normal font-sans text-center px-5 ">
@@ -31,7 +31,7 @@ const Footer = () => {
             </p>
             <p className="text-sm lg:text-lg text-[#201A59] font-normal font-sans text-center px-5 ">
               +4407848938273
-            </p>
+            </p> */}
           </div>
 
           <div>
