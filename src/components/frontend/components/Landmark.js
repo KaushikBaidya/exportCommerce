6e774a5bed7a06f83eb4c621a3036de12fc8d481
KAUSHIK BAIDYA <47146123+KaushikBaidya@users.noreply.github.com>
2022-08-06 10:06:29 +0600

    Update Landmark.js

--- src/components/frontend/components/Landmark.js
+++ src/components/frontend/components/Landmark.js
@@ -18,10 +18,33 @@ function Landmark() {
             About{" "}
             <span className="md:text-2xl lg:text-5xl text-[#ED1A37]">TVC</span>
           </h1>
-          <p className="text-left">
-            The TVC VIETNAM online marketplace, which is a registered trademark
-            and must register the terms and conditions of the site with the
-            Patent and Trademark Office, was one of three sellers on market.
+          <p className="text-justify">
+            Climate change is a global challenge and fighting it requires
+            coordinated actions by all countries in the world. Bioenergy is a
+            key strategy to reduce carbon emissions and limit dependence on
+            fossil fuels.
+          </p>
+          <p className="text-justify">
+            Since Vietnam has the advantage in large sustainably planted forest
+            areas, it is a potential country to develop strong logging and
+            biomass energy industry.{" "}
+          </p>
+          <p className="text-justify">
+            TVC VietNam is founded to play a key role in reducing the emissions
+            that contribute to global warming and climate change by producing
+            wood products such as wood pellets, rice husk briquette, sawdust
+            briquette, bamboo sawdust briquette, and charcoal….
+          </p>
+          <p className="text-justify">
+            We have a lot of experience in exporting products such as Wood
+            pellets, charcoal, chip block, and rice husk briquettes….. to
+            markets such as China, Korea, India, Japan, and fastidious markets.
+            like Europe.
+          </p>
+          <p className="text-justify">
+            We wish all the citizens of the world have a lot of happiness and
+            healthy, together with us fighting climate change by replacing coal
+            and encouraging afforestation to protect the environment.
           </p>
         </div>
       </div>
