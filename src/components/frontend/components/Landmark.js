ffe14574a8d959c965aab22f40f3dc15bf07d66d
KAUSHIK BAIDYA <47146123+KaushikBaidya@users.noreply.github.com>
2022-08-06 15:40:50 +0600

    updated

--- src/components/frontend/components/Landmark.js
+++ src/components/frontend/components/Landmark.js
@@ -4,52 +4,60 @@ import React from "react";
 function Landmark() {
   // const { t } = useTranslation(["landmark"]);
   return (
-    <section id="seeAbout" className="py-20 h-full">
-      <div className="grid grid-cols-1 md:grid-cols-2 px-6 gap-x-6 items-center">
-        <div>
-          <img
-            src="https://fireplaceuniverse.com/wp-content/uploads/pellet-stove-pellets-burning.jpg"
-            className="rounded-lg w-5/6 mx-auto"
-            alt=""
-          />
-        </div>
-        <div className="text-black grid md:text-lg gap-y-5 mt-2 md:mt-0">
-          <h1 className="font-bold text-xl md:text-xl lg:text-4xl font-mono text-center">
-            <span className="md:text-2xl lg:text-5xl text-[#36B34B]">
-              About{" "}
-            </span>
-            <span className="md:text-2xl lg:text-5xl text-[#F5921E]">Us</span>
-          </h1>
-          <p className="text-justify ">
+    <section
+      id="seeAbout"
+      className="grid grid-cols-1 px-6 justify-items-center py-20 h-full"
+    >
+      <h1 className="text-4xl font-bold mb-5">
+        <span className="text-[#36B34B]">About </span>
+        <span className="text-[#F5921E]">US</span>
+      </h1>
+      <div className="max-w-5xl grid grid-cols-1 md:grid-cols-2 justify-items-center gap-5 my-5">
+        <img
+          src="https://fireplaceuniverse.com/wp-content/uploads/pellet-stove-pellets-burning.jpg"
+          className="h-56 rounded-lg"
+          alt=""
+        />
+        <div className="flex flex-col text-lg">
+          <p className="text-justify my-2">
             Climate change is a global challenge and fighting it requires
             coordinated actions by all countries in the world. Bioenergy is a
             key strategy to reduce carbon emissions and limit dependence on
             fossil fuels.
           </p>
-          <p className="text-justify">
+          <p className="text-justify mb-2">
             Since Vietnam has the advantage in large sustainably planted forest
             areas, it is a potential country to develop strong logging and
             biomass energy industry.{" "}
           </p>
-          <p className="text-justify">
+        </div>
+      </div>
+      <div className="max-w-5xl grid grid-cols-1 md:grid-cols-2 justify-items-center gap-5 my-5">
+        <div className="flex flex-col text-lg">
+          <p className="text-justify my-2">
             TVC VietNam is founded to play a key role in reducing the emissions
             that contribute to global warming and climate change by producing
             wood products such as wood pellets, rice husk briquette, sawdust
             briquette, bamboo sawdust briquette, and charcoal….
           </p>
-          <p className="text-justify">
+          <p className="text-justify mb-2">
             We have a lot of experience in exporting products such as Wood
             pellets, charcoal, chip block, and rice husk briquettes….. to
             markets such as China, Korea, India, Japan, and fastidious markets.
             like Europe.
           </p>
-          <p className="text-justify">
-            We wish all the citizens of the world have a lot of happiness and
-            healthy, together with us fighting climate change by replacing coal
-            and encouraging afforestation to protect the environment.
-          </p>
         </div>
+        <img
+          src="https://fireplaceuniverse.com/wp-content/uploads/pellet-stove-pellets-burning.jpg"
+          className="h-56 rounded-lg"
+          alt=""
+        />
       </div>
+      <p className="max-w-5xl text-justify text-lg my-5">
+        We wish all the citizens of the world have a lot of happiness and
+        healthy, together with us fighting climate change by replacing coal and
+        encouraging afforestation to protect the environment.
+      </p>
     </section>
   );
 }
--- src/components/frontend/components/Products.js
+++ src/components/frontend/components/Products.js
@@ -16,7 +16,7 @@ function Products() {
           className="mx-auto grid grid-cols-1 gap-10 lg:px-14 py-20 text-dark"
         >
           <div className="text-center font-semibold text-4xl text-[#36B34B]">
-            Our Products
+            Our <span className="text-[#F5921E] ">Products</span>
           </div>
 
           <div className="px-5 lg:px-14 grid grid-cols-1 md:grid-cols-3 gap-4">
@@ -28,7 +28,7 @@ function Products() {
                 <img src={item.img} className="h-72 rounded-lg" alt="" />
                 <div className="mt-2">
                   <Link to={item.id}>
-                    <h1 className="text-lg text-center font-medium text-[#36B34B] px-4">
+                    <h1 className="text-lg text-center font-medium ext-black hover:text-[#36B34B] px-4">
                       {item.title}
                     </h1>
                   </Link>
@@ -42,34 +42,6 @@ function Products() {
               </div>
             ))}
           </div>
-          {/* <div className="grid grid-cols-1 md:grid-cols-2 px-4 content-center justify-items-center gap-x-2">
-            <div className="w-full min-h-20 grid grid-cols-2 content-center justify-items-center bg-yellow-200 rounded-lg mt-2">
-              <div>
-                <img
-                  src="https://fireplaceuniverse.com/wp-content/uploads/pellet-stove-pellets-burning.jpg"
-                  className="rounded-lg w-5/6 p-5 mx-auto"
-                  alt=""
-                />
-              </div>
-              <div className="text-black grid content-center justify-items-center gap-y-2 mt-2 md:mt-0">
-                <h1 className="text-base font-semibold">Biomass Wood Pellet</h1>
-                <p className="text-base font-semibold">$12/ Kilogram</p>
-              </div>
-            </div>
-            <div className="w-full min-h-20 grid grid-cols-2 content-center justify-items-center bg-blue-200 rounded-lg mt-2">
-              <div>
-                <img
-                  src="https://fireplaceuniverse.com/wp-content/uploads/pellet-stove-pellets-burning.jpg"
-                  className="rounded-lg w-5/6 p-5 mx-auto"
-                  alt=""
-                />
-              </div>
-              <div className="text-black grid content-center justify-items-center gap-y-2 mt-2 md:mt-0">
-                <h1 className="text-base font-semibold">Biomass Wood Pellet</h1>
-                <p className="text-base font-semibold">$12/ Kilogram</p>
-              </div>
-            </div>
-          </div> */}
         </div>
       </section>
     </>
